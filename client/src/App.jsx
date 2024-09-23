5c9d0dedb54c2f3a4fb1f5d29a5e10dd1800d5ea
kwingham <kyle_wingham@outlook.com>
2024-09-23 21:25:02 +0100

    Mainly tweaked the styling and added the sad rooster image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ClickTracker from "./components/ClickTracker";
 import "./styles/App.css";
+import sadRooster from "./assets/sad-rooster.webp";
 
 function App() {
   return (
     <div className="app-container">
       <h1>Rooster Chick Chase</h1>
       <ClickTracker />
+      <img src={sadRooster} alt="Sad Rooster" className="sad-rooster" />{" "}
     </div>
   );
 }
